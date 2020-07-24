11abf2d3f031f58891d657ba8f0732d5cd49aca1
JevF1982 <40211448+JevF1982@users.noreply.github.com>
2020-07-24 15:17:34 +0200

    production deploy

---
+++ Frontend/build/precache-manifest.991df974825cc9f91c733e01e3da81ed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9020e98225f2e3f66948c791fcf1df2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bb18b887dedfbac8839",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "51609a420f29fb5f8ff2",
+    "url": "/static/css/main.f04fe543.chunk.css"
+  },
+  {
+    "revision": "9bb18b887dedfbac8839",
+    "url": "/static/js/2.86b1bc38.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.86b1bc38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51609a420f29fb5f8ff2",
+    "url": "/static/js/main.108638a3.chunk.js"
+  },
+  {
+    "revision": "fd07fd9b8c706602eaf4",
+    "url": "/static/js/runtime-main.3db53691.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- app.js
+++ app.js
@@ -32,6 +32,10 @@ mongoose
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("Frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
 
 // listen on port
